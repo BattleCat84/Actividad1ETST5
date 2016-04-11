af2604724ffd7760e9b409a2d386616742b90858
BattleCat84 <migueli_jrr@hotmail.es>
2016-04-11 20:17:24 +0100

    commit 4

---
+++ src/dam_ed04_actividad/Empleado.java
@@ -0,0 +1,16 @@
+package dam_ed04_actividad;
+
+/**
+ *
+ * @author Migueli Ramos
+ */
+public class Empleado {
+    
+//Este es el commit4
+    
+    String nombre;
+    String apellidos;
+    String dni;
+    
+    
+}
