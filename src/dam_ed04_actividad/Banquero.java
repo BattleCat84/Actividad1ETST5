b4c54a51f937df7ea62c2f51a3851eecfeb5c7d0
BattleCat84 <migueli_jrr@hotmail.es>
2016-04-12 16:18:52 +0100

    commit7

--- src/dam_ed04_actividad/Banquero.java
+++ src/dam_ed04_actividad/Banquero.java
@@ -5,8 +5,25 @@ package dam_ed04_actividad;
  * @author Migueli Ramos
  */
 
+ //Este es el sexto commit
+
 public class Banquero {
 
- //Este es el sexto commit
+// Este es el septimo commit
     
-}
+    private String banqueroNombre;
+    
+    public String getBanqueroNombre(){
+        
+        return banqueroNombre;
+    }   
+        
+        public void setBanqueroNombre(String banqueroNombre) {
+            
+            this.banqueroNombre = banqueroNombre;
+        }
+        
+    }
+    
+    
+
